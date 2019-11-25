1000d907f63377239bc2ef5f6883820a035ef8cd
Shusuke Sato <sato.fun0619@gmail.com>
2019-11-25 10:59:45 +0900

    Initial Commit

---
+++ bbtagApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bbtagApp
+//
+//  Created by 佐藤秀輔 on 2019/11/25.
+//  Copyright © 2019 佐藤秀輔. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
